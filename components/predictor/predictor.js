b075800764663e641c43a6c251bab22e6938be80
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-11 10:57:34 +0000

    predictor component holding search and result

---
+++ components/predictor/predictor.js
@@ -0,0 +1,59 @@
+import { useContext, useState, Fragment } from "react";
+import classes from "./predictor.module.css";
+import NotificationContext from "../../store/notification-context";
+import MakePrediction from "../prediction-items/make-prediction";
+import PredictionResult from "../prediction-items/prediction-result";
+
+function Predictor() {
+  const notificationCtx = useContext(NotificationContext);
+  const [prediction, setPrediction] = useState(null);
+
+  function makePredictionHandler(stockSymbol) {
+    notificationCtx.showNotification({
+      title: "Fetching...",
+      message: "Making prediction...",
+      status: "pending...",
+    });
+    fetch("/api/predictor", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        stockSymbol: stockSymbol,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.json().then((error) => {
+          throw new Error(error.message || "Something went wrong");
+        });
+      })
+      .then((data) => {
+        setPrediction(data[0]);
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Prediction made successfully!",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong!",
+          status: "error",
+        });
+      });
+  }
+
+  return (
+    <div className={classes.predictionWrapper}>
+      <MakePrediction onMakePrediction={makePredictionHandler} />
+      {prediction && <PredictionResult prediction={prediction} />}
+    </div>
+  );
+}
+
+export default Predictor;
