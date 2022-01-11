2934d96fc7edca3b047db52fd95c1ff5af69285d
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-11 15:15:14 +0000

    update request to api with correct params and error handling

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,10 +1,18 @@
 import { getDummyPrediction } from "../dummy-data";
 
 export async function makeNewPrediction(props) {
-  const URL = `http://localhost:8080/api/v1/predict/${props.enteredStock}`;
-  const response = await fetch(URL)
+  const URL = `http://localhost:8080/api/v1/predict/${props}`;
+  const response = await fetch(URL, {
+    method: "GET",
+  })
     .then((res) => res.json())
-    .then((data) => data);
+    .then((data) => {
+      if (data.message === "Stock not found") {
+        return { message: "Stock not found" };
+      } else {
+        return data;
+      }
+    });
   return response;
 }
 
