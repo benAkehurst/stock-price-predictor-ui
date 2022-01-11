ea3d41dea0e29d8cb168d5c69aa9841fb4619f8d
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-11 10:58:02 +0000

    prediction result visual component

---
+++ components/prediction-items/prediction-result.js
@@ -0,0 +1,44 @@
+import classes from "./prediction-result.module.css";
+
+function PredictionResult(props) {
+  console.log(props.prediction);
+  return (
+    <div className={classes.predictionResult}>
+      <div className={classes.banner}>
+        <h2>Prediction Result</h2>
+      </div>
+      <div className={classes.predictionResults}>
+        <div className={classes.row}>
+          <div className={classes.predictionResultItem}>
+            <h3>Open Price</h3>
+            <p>{props.prediction.predictionData.open.toFixed(2)}</p>
+          </div>
+          <div className={classes.predictionResultItem}>
+            <h3>Close Price</h3>
+            <p>{props.prediction.predictionData.close.toFixed(2)}</p>
+          </div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.predictionResultItem}>
+            <h3>High Price</h3>
+            <p>{props.prediction.predictionData.high.toFixed(2)}</p>
+          </div>
+          <div className={classes.predictionResultItem}>
+            <h3>Low Price</h3>
+            <p>{props.prediction.predictionData.low.toFixed(2)}</p>
+          </div>
+        </div>
+        <div className={classes.predictionResultItem}>
+          <h3>Price Trend</h3>
+          <p>{props.prediction.priceTrend.trend}</p>
+        </div>
+        <div className={classes.predictionResultItem}>
+          <h3>Prediction Time Taken</h3>
+          <p>{props.prediction.predictionTimeTaken.toFixed(2)} seconds</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PredictionResult;
