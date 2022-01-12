b726e60d453f52a87a8abc521c6e31eb4bd08de5
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-12 14:32:17 +0000

    show past predictions

--- components/prediction-items/prediction-result.js
+++ components/prediction-items/prediction-result.js
@@ -1,12 +1,19 @@
+import { parseISO, format } from "date-fns";
+
 import classes from "./prediction-result.module.css";
 
 function PredictionResult(props) {
+  const result = parseISO(props.prediction.predictionMadeOnDate);
+  const date = format(result, "dd MMM YYY");
   return (
     <div className={classes.predictionResult}>
       <div className={classes.banner}>
         <h2>Prediction Result</h2>
       </div>
       <div className={classes.predictionResults}>
+        <div className={classes.row}>
+          Company - {props.prediction.stockSymbol}
+        </div>
         <div className={classes.row}>
           <div className={classes.predictionResultItem}>
             <h3>Open Price</h3>
@@ -30,6 +37,10 @@ function PredictionResult(props) {
         <div className={classes.predictionResultItem}>
           <h3>Price Trend</h3>
           <p>{props.prediction.priceTrend.trend}</p>
+        </div>
+        <div className={classes.row}>
+          <h3>Date Made</h3>
+          <p>{date}</p>
         </div>
         <div className={classes.predictionResultItem}>
           <h3>Prediction Time Taken</h3>
--- components/show-prediction/show-prediction.js
+++ components/show-prediction/show-prediction.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import classes from "./show-prediction.module.css";
 import PredictionResult from "../prediction-items/prediction-result";
 
 function ShowPrediction() {
+  const [predictionData, setPredictionData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/pastPredictions", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        setPredictionData(data.pastPredictions);
+      });
+  }, []);
+
   return (
-    <div className={classes.predictionsWrapper}>Show Past Predictions</div>
+    <div className={classes.predictionsWrapper}>
+      {predictionData &&
+        predictionData.slice(0, 3).map((prediction) => {
+          return (
+            <PredictionResult key={prediction._id} prediction={prediction} />
+          );
+        })}
+    </div>
   );
 }
 
