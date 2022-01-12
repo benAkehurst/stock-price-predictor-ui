d4ec725ed713baf823f2ebb5f9cc42a0a5593d49
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-12 11:51:12 +0000

    show predictions module

---
+++ components/show-prediction/show-prediction.js
@@ -0,0 +1,10 @@
+import classes from "./show-prediction.module.css";
+import PredictionResult from "../prediction-items/prediction-result";
+
+function ShowPrediction() {
+  return (
+    <div className={classes.predictionsWrapper}>Show Past Predictions</div>
+  );
+}
+
+export default ShowPrediction;
