5d0219d2bbe6cdda6696e6e0b33ce475957b8b13
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-12 14:34:55 +0000

    add show loading state

--- components/show-prediction/show-prediction.js
+++ components/show-prediction/show-prediction.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
 import classes from "./show-prediction.module.css";
 import PredictionResult from "../prediction-items/prediction-result";
+import PredictingProcessing from "../ui/predicting-processing";
 
 function ShowPrediction() {
+  const [isLoading, setIsLoading] = useState(false);
   const [predictionData, setPredictionData] = useState([]);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch("/api/pastPredictions", { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
+        setIsLoading(false);
         setPredictionData(data.pastPredictions);
       });
   }, []);
 
   return (
     <div className={classes.predictionsWrapper}>
+      {isLoading && <PredictingProcessing />}
       {predictionData &&
         predictionData.slice(0, 3).map((prediction) => {
           return (
