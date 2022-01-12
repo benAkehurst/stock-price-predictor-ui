f60fd1e74b82c9732d5e258b85402024c4c7b2c2
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-12 16:18:43 +0000

    prediction result styling

--- components/prediction-items/prediction-result.js
+++ components/prediction-items/prediction-result.js
@@ -1,49 +1,78 @@
 import { parseISO, format } from "date-fns";
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import classes from "./prediction-result.module.css";
 
 function PredictionResult(props) {
   const result = parseISO(props.prediction.predictionMadeOnDate);
   const date = format(result, "dd MMM YYY");
+
+  const currencySymbol = props.prediction.stockSymbol.includes(".lon")
+    ? "£"
+    : "$";
+
   return (
-    <div className={classes.predictionResult}>
-      <div className={classes.banner}>
-        <h2>Prediction Result</h2>
-      </div>
-      <div className={classes.predictionResults}>
-        <div className={classes.row}>
+    <div className={classes.predictionResultWrapper}>
+      <div className={classes.predictionResult}>
+        <div className={classes.banner}>
           Company - {props.prediction.stockSymbol}
         </div>
         <div className={classes.row}>
           <div className={classes.predictionResultItem}>
             <h3>Open Price</h3>
-            <p>{props.prediction.predictionData.open.toFixed(2)}</p>
+            <p>
+              {currencySymbol}
+              {props.prediction.predictionData.open.toFixed(2)}
+            </p>
           </div>
           <div className={classes.predictionResultItem}>
             <h3>Close Price</h3>
-            <p>{props.prediction.predictionData.close.toFixed(2)}</p>
+            <p>
+              {currencySymbol}
+              {props.prediction.predictionData.close.toFixed(2)}
+            </p>
           </div>
         </div>
         <div className={classes.row}>
           <div className={classes.predictionResultItem}>
             <h3>High Price</h3>
-            <p>{props.prediction.predictionData.high.toFixed(2)}</p>
+            <p>
+              {currencySymbol}
+              {props.prediction.predictionData.high.toFixed(2)}
+            </p>
           </div>
           <div className={classes.predictionResultItem}>
             <h3>Low Price</h3>
-            <p>{props.prediction.predictionData.low.toFixed(2)}</p>
+            <p>
+              {currencySymbol}
+              {props.prediction.predictionData.low.toFixed(2)}
+            </p>
           </div>
         </div>
-        <div className={classes.predictionResultItem}>
+        <div className={classes.row}>
           <h3>Price Trend</h3>
-          <p>{props.prediction.priceTrend.trend}</p>
+          <p
+            className={
+              props.prediction.priceTrend.trend === "up"
+                ? classes.green
+                : classes.red
+            }
+          >
+            {props.prediction.priceTrend.trend === "up" ? (
+              <FontAwesomeIcon icon={faArrowUp} />
+            ) : (
+              <FontAwesomeIcon icon={faArrowDown} />
+            )}{" "}
+            {props.prediction.priceTrend.trend.toUpperCase()}
+          </p>
         </div>
         <div className={classes.row}>
-          <h3>Date Made</h3>
+          <h3>Prediction Made On:</h3>
           <p>{date}</p>
         </div>
-        <div className={classes.predictionResultItem}>
-          <h3>Prediction Time Taken</h3>
+        <div className={classes.row}>
+          <h3>Prediction Time Taken:</h3>
           <p>{props.prediction.predictionTimeTaken.toFixed(2)} seconds</p>
         </div>
       </div>
