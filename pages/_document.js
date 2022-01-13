3a70d7943277ad5daa59030d75e680432b070666
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-13 14:44:48 +0000

    add modal div

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head />
         <body>
-          <div id='overlays' />
+          <div id="overlays" />
           <Main />
           <NextScript />
+          <div id="modal-root"></div>
         </body>
       </Html>
     );
