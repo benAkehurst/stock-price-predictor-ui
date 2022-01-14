53da0c4ad421df17c9f2dacaad240738a1683820
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-14 16:26:10 +0000

    use provider and new protected pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,26 @@
 import Head from "next/head";
-
+import { SessionProvider } from "next-auth/react";
 import Layout from "../components/layout/layout";
 import { NotificationContextProvider } from "../store/notification-context";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <NotificationContextProvider>
-      <Layout>
-        <Head>
-          <title>Stock Price Predictor</title>
-          <meta name="description" content="Stock price predictor" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
-    </NotificationContextProvider>
+    <SessionProvider session={session}>
+      <NotificationContextProvider>
+        <Layout>
+          <Head>
+            <title>Stock Price Predictor</title>
+            <meta name="description" content="Stock price predictor" />
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </Layout>
+      </NotificationContextProvider>
+    </SessionProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from "next/head";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Predictor from "../components/predictor/Predictor";
 import ShowPrediction from "../components/show-prediction/show-prediction";
 
 function HomePage() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <div>
+        Welcome user
+        <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  }
   return (
     <div>
       <Head>
@@ -12,6 +23,10 @@ function HomePage() {
           content="An app that uses ML to calculate stock prices for the coming day"
         />
       </Head>
+      <div>
+        Click to sign into your user account <br />
+        <button onClick={() => signIn()}>Sign in</button>
+      </div>
       <div className="main-items-wrapper">
         <Predictor />
         <ShowPrediction />
