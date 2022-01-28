d5073075ec069910b776dc87716610fd0bad71e8
Ben <ben.akehurst@sainsburys.co.uk>
2022-01-28 17:01:54 +0000

    add container and update copy

--- components/UI/LandingPage.tsx
+++ components/UI/LandingPage.tsx
@@ -1,15 +1,18 @@
+import { Container } from "@mantine/core";
+
 export default function LandingPage() {
   return (
-    <div>
+    <Container>
       <h1>Welcome to Stock Price Predictor.</h1>
       <p>
         This is a web application that predicts the stock price of a company
-        using Machine Learning in real time!
+        using Machine Learning in real time and you'll have a prediction of what
+        the stock market is going to do!
       </p>
       <p>
         To get started, please click the button above to sign in using your
         Google Account.
       </p>
-    </div>
+    </Container>
   );
 }
